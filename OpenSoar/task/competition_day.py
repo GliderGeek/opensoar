9c67714ddb6767461ee7a34025c6b9b5aebf900d
GliderGeek <GliderGeek@users.noreply.github.com>
2017-12-17 22:05:37 +0100

    add competitor and competition_day classes, this closes #7 and closes #8

---
+++ OpenSoar/task/competition_day.py
@@ -0,0 +1,18 @@
+import datetime
+from typing import List
+
+from OpenSoar.task.competitor import Competitor
+from OpenSoar.task.task import Task
+
+
+class CompetitionDay:
+
+    def __init__(self, competitors: List[Competitor], task: Task, date: datetime.date):
+
+        self._competitors = competitors
+        self._task = task
+        self._date = date
+
+    def analyse_flights(self):
+        for competitor in self._competitors:
+            competitor.analyse(self._task)
---
+++ OpenSoar/task/competitor.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from OpenSoar.task.trip import Trip
+from OpenSoar.thermals.flight_phases import FlightPhases
+
+
+class Competitor:
+
+    def __init__(self, trace: List, competition_id: str=None, airplane: str=None, ranking: int=None, pilot: str=None):
+        self._trace = trace
+        self._competition_id = competition_id
+        self._airplane = airplane
+        self._ranking = ranking
+        self._pilot = pilot
+
+        # to be set by analyse method
+        self._trip = None
+        self._phases = None
+        # todo: should this also contain performance?
+        # for comparison reasons it would make more sense to store this on comp level
+
+    def analyse(self, task):
+        self._trip = Trip(task, self._trace)
+
+        # competitor should have at least started
+        if self._trip.fixes >= 1:
+            self._phases = FlightPhases(self._trip, self._trace)

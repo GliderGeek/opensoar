fe0c4e4e39b53b1926dd4594b4b31dbd8066ad38
GliderGeek <GliderGeek@users.noreply.github.com>
2018-02-17 23:24:48 +0100

    attempt to fix opensoar dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='opensoar',
     version='0.1.0',
     description='Open source python library for glider flight analysis',
     url='https://github.com/glidergeek/opensoar',
-    packages=find_packages(),
+    packages=['pygeodesy', 'aerofiles', 'beautifulsoup4'],
 )

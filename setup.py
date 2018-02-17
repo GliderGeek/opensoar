6cf8e36888da14cbb91eaab66d0575a30f54171a
GliderGeek <GliderGeek@users.noreply.github.com>
2018-02-17 23:30:34 +0100

    change setup

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='opensoar',
     version='0.1.0',
     description='Open source python library for glider flight analysis',
     url='https://github.com/glidergeek/opensoar',
-    packages=['pygeodesy', 'aerofiles', 'beautifulsoup4'],
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'pygeodesy>=17.11.26',
+        'aerofiles>=0.4',
+        'beautifulsoup4>=4.6.0'
+    ]
 )

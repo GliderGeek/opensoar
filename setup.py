eab8a0c530e9adbba6e0afe9679414cc269a5056
GliderGeek <GliderGeek@users.noreply.github.com>
2018-02-17 20:59:38 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='opensoar',
+    version='0.1.0',
+    description='Open source python library for glider flight analysis',
+    url='https://github.com/glidergeek/opensoar',
+    packages=find_packages(),
+)

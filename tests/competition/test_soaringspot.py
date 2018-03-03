fb6e95025379f098e0d4fafb4a644de1e48789e6
GliderGeek <GliderGeek@users.noreply.github.com>
2018-03-03 17:14:55 +0100

    fix mistake in test

--- tests/competition/test_soaringspot.py
+++ tests/competition/test_soaringspot.py
@@ -75,7 +75,7 @@ class TestSoaringspot(unittest.TestCase):
         soaringspot_page = SoaringSpotDaily(
             'https://www.soaringspot.com/en/sallandse-tweedaagse-2014/results/club/task-1-on-2014-06-21/daily', '')
 
-        competitor_pk = soaringspot_page.get_competitors()[2]
+        competitor_pk = soaringspot_page.competitors[2]
         self.assertEqual(competitor_pk.competition_id, 'PK')
 
         self.assertEqual(competitor_pk.ranking, 3)
@@ -87,7 +87,7 @@ class TestSoaringspot(unittest.TestCase):
         soaringspot_page = SoaringSpotDaily(
             'https://www.soaringspot.com/en/sallandse-tweedaagse-2014/results/club/task-1-on-2014-06-21/daily', '')
 
-        competitionday = soaringspot_page.get_competition_day()
+        competitionday = soaringspot_page.competition_day()
 
         self.assertEqual(competitionday.name, 'sallandse-tweedaagse-2014')
         self.assertEqual(competitionday.plane_class, 'club')

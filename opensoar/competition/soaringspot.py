eddf77b1a31412f01329d2e01cfcf8c9437b9e72
GliderGeek <GliderGeek@users.noreply.github.com>
2018-06-16 17:17:07 +0200

    try both utf-8 and latin1 for decoding file

--- opensoar/competition/soaringspot.py
+++ opensoar/competition/soaringspot.py
@@ -317,8 +317,12 @@ class SoaringSpotDaily(DailyResultsPage):
             if download_progress is not None:
                 download_progress(files_downloaded, len(competitors_info))
 
-            with open(file_path, 'r', encoding='utf-8') as f:
-                parsed_igc_file = Reader().read(f)
+            try:
+                with open(file_path, 'r', encoding='utf-8') as f:
+                    parsed_igc_file = Reader().read(f)
+            except UnicodeDecodeError:
+                with open(file_path, 'r', encoding='latin1') as f:
+                    parsed_igc_file = Reader().read(f)
 
             trace_errors, trace = parsed_igc_file['fix_records']
 
--- opensoar/competition/strepla.py
+++ opensoar/competition/strepla.py
@@ -273,8 +273,12 @@ class StreplaDaily(DailyResultsPage):
             if download_progress is not None:
                 download_progress(files_downloaded, len(table_info))
 
-            with open(file_path, 'r', encoding='utf-8') as f:
-                parsed_igc_file = Reader().read(f)
+            try:
+                with open(file_path, 'r', encoding='utf-8') as f:
+                    parsed_igc_file = Reader().read(f)
+            except UnicodeDecodeError:
+                with open(file_path, 'r', encoding='latin1') as f:
+                    parsed_igc_file = Reader().read(f)
 
             trace_errors, trace = parsed_igc_file['fix_records']
 

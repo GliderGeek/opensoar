3ffaa22c27000522428ed4692e66397bba7bcddb
GliderGeek <GliderGeek@users.noreply.github.com>
2018-06-19 17:57:40 +0200

    improve setup.py: - add long description - add version number in file

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
+exec(open('opensoar/version.py').read())
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
 setup(
     name='opensoar',
-    version='0.1.0',
+    version=__version__,  # has been import above in exec command
     license='MIT',
     description='Open source python library for glider flight analysis',
     url='https://github.com/glidergeek/opensoar',
     packages=find_packages(exclude=['tests']),
+    long_description=long_description,
     install_requires=[
         'pygeodesy>=17.11.26',
         'aerofiles>=0.4.1',
